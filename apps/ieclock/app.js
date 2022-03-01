c7d36844ee220174896cad26cfea1530d0716580
indridi <indridi@kvikna.com>
2022-03-01 01:11:32 +0000

    update clock

--- apps/ieclock/app.js
+++ apps/ieclock/app.js
@@ -4,16 +4,16 @@
 // 3/4 : String.fromCharCode(190)
 baro = {};
 baro.draw = function draw(x,y,Radius, Settings) {
-  let halfScreenWidth   = g.getWidth() / 2;
-  let largeComplication = (x === halfScreenWidth);
-  g.setColor(Settings.Foreground === 'Theme' ? g.theme.fg : Settings.Foreground || '#000000');
-  g.setFont('Vector', 18);
-  g.setFontAlign(0,0);
-  dp = Math.round(WIDGETS.widbarom.getChange() * 10) / 10;
-  p = Math.round(WIDGETS.widbarom.getLastPressure().pressure);
+    let halfScreenWidth   = g.getWidth() / 2;
+    let largeComplication = (x === halfScreenWidth);
+    g.setColor(Settings.Foreground === 'Theme' ? g.theme.fg : Settings.Foreground || '#000000');
+    g.setFont('Vector', 18);
+    g.setFontAlign(0,0);
+    dp = Math.round(WIDGETS.widbarom.getChange() * 10) / 10;
+    p = Math.round(WIDGETS.widbarom.getLastPressure().pressure);
     dpsign = (dp<0?"-":"+") + dp;
-  Text = ''+p+dpsign+"Hp";
-  g.drawString(Text, x,y);
+    Text = ''+p+dpsign+"Hp";
+    g.drawString(Text, x,y);
 };
 
 require('https://raw.githubusercontent.com/rozek/banglejs-2-widgets-on-background/main/drawWidgets.js');
@@ -29,5 +29,5 @@ Clockwork.windUp({
         r:require('https://raw.githubusercontent.com/rozek/banglejs-2-moon-phase-complication/main/Complication.js'),
         t:require('https://raw.githubusercontent.com/rozek/banglejs-2-date-complication/main/Complication.js'),
         b:baro
-  }
+    }
 }, {'Foreground':'Theme', 'Background':'Theme'});
