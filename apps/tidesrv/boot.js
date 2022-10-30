c7a3635381ab9039c3723d1dae5150b84594c48c
indridi <indridi@kvikna.com>
2022-10-30 23:36:46 +0000

    tide service

---
+++ apps/tidesrv/boot.js
@@ -0,0 +1,88 @@
+function updateSunRiseSunSet(){
+  var SunCalc = require("https://raw.githubusercontent.com/mourner/suncalc/master/suncalc.js");
+  let now=Date();
+  let location = require("Storage").readJSON(tidesrv.LOCATION_FILE,1)||{};
+  location.lat = location.lat||51.5072;
+  location.lon = location.lon||0.1276;
+  location.location = location.location||"London";
+  let times = SunCalc.getTimes(new Date(), location.lat, location.lon);
+  tidesrv.times = times;
+  tidesrv.sunrise = times.sunrise;
+  tidesrv.sunset = times.sunset;
+}
+
+function refreshTideFile() {
+  let settings = require('Storage').readJSON("tidesrv.json", true) || {};
+  let tidesite = settings.tidesite || 'reykjavik';
+  delete settings; // remove unneeded settings from memory
+
+  let now = new Date();
+  let start = now-24*60*60*1000;
+  let end = now+24*60*60*1000*30;
+  let str = 'https://tideapi.spliff-donk.de/stations/table?stationid='+tidesite+'&startdate='+start.getFullYear()+'-'+(start.getMonth()+1)+'-'+start.getDate()+'&enddate='+end.getFullYear()+'-'+(end.getMonth()+1)+'-'+end.getDate();
+  Bangle.http(str).then(data=>{
+    console.log("Got ",data);
+    require("Storage").write(tidesrv.TIDE_FILE, data);
+  });
+}
+
+function updateTide() {
+  nowT=Date().getTime();
+  f = require("Storage").read(tidesrv.TIDE_FILE);
+  if (typeof f == 'undefined'){
+    return;
+  }
+  f=f.split(";");
+  for (ix in f)
+  {
+    e = f[ix].split(",");
+    t = e[0];
+    if(t > nowT){
+      var tideinfo = e;
+      var lasttide = laste;
+      break;
+    }
+    laste = e;
+  }
+  if (typeof tideinfo == 'undefined'){
+    return;
+  }
+  if (typeof laste == 'undefined'){
+    return;
+  }
+  th = Math.round(parseInt(e[1])/10)/10;
+  thlast = Math.round(parseInt(laste[1])/10)/10;
+  tidesrv.tides = {
+    'time': Date(parseInt(e[0])),
+    'timestamp':parseInt(e[0]),
+    'e':e,
+    'height':th,
+    'lastheight':thlast, 
+    'timelast':parseInt(laste[0]),
+    'high':(e[2]=='true'?true:false)
+  };
+}
+
+
+
+global.tidesrv = {
+  sunrise: Date(),
+  sunset: Date(),
+  location: {},
+  times: {},
+  tides: {},
+  LOCATION_FILE: "mylocation.json",
+  TIDE_FILE: "tides.data.csv",
+  intervalId: -1,
+  updateTide: updateTide,
+  updateSunRiseSunSet: updateSunRiseSunSet,
+  refreshTideFile: refreshTideFile,
+}; 
+
+updateSunRiseSunSet();
+updateTide();
+
+tidesrv.intervaldId = setInterval(function() {
+    updateSunRiseSunSet();
+    updateTide();
+}, 1000*60*60*2); // update occasionally
---
+++ apps/tidesrv/settings.js
@@ -0,0 +1,32 @@
+(function(back) {
+  stuff=JSON.parse('[{"id":"thorlakshofn","description":"Þorlákshöfn"},{"id":"grindavik","description":"Grindavík"},{"id":"reykjanesta","description":"Reykjanestá/Röst"},{"id":"sandvik","description":"Sandvík"},{"id":"gardskagi","description":"Garðskagi"},{"id":"reykjavik","description":"Reykjavík"},{"id":"hvalfjordur","description":"Hvalfjörður"},{"id":"grundartangi","description":"Grundartangi"},{"id":"borgarfjordur","description":"Borgarfjörður"},{"id":"longufjorur","description":"Löngufjörur"},{"id":"arnarstapi","description":"Arnarstapi"},{"id":"hornvik","description":"Hornvík"},{"id":"latravik","description":"Látravik"},{"id":"straumnes","description":"Straumnes"},{"id":"holmavik","description":"Hólmavík"},{"id":"husavik","description":"Húsavík"},{"id":"testsite","description":"Testsite"}]')
+  
+  var FILE = "tidesrv.json";
+  // Load settings
+  var settings = Object.assign({
+    tidesite: 'reykjavik',
+    nr: 6
+  }, require('Storage').readJSON(FILE, true) || {});
+
+  function writeSettings() {
+    require('Storage').writeJSON(FILE, settings);
+  }
+
+  // Show the menu
+  E.showMenu({
+    "" : { "title" : "App Name" },
+    "< Back" : () => back(),
+    'Site?': {
+      value: 0|settings.id,  // 0| converts undefined to 0
+      min: 0, max: stuff.length-1,
+      format : v => stuff[v].id,
+      onchange: v => {
+        console.log(v);
+        settings.tidesite = stuff[v].id;
+        settings.nr = v;
+        writeSettings();
+        global.tidesrv.refreshTideFile();
+      }
+    },
+  });
+})
